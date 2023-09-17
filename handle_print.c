f88142797f84e7281a4e6331c5583e11a4699e70
Tobore1 <70794182+Tobore1@users.noreply.github.com>
2023-09-17 10:15:08 +0100

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * handle_print - Prints an argument based on its type
- * @fmt: Formatted string in which to print the arguments.
- * @list: List of arguments to be printed.
+ * handleis_print - this will Prints an argument based on its type
+ * @fmt: this will Formatted string in which to print the arguments.
+ * @list: will print the List of arguments.
  * @ind: ind.
  * @buffer: Buffer array to handle print.
  * @flags: Calculates active flags
