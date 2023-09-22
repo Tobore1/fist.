f3176a288894ed22b1564f397f5da612f9e30d67
Tobore1 <ogbodutobore@gmail.com>
2023-09-21 21:19:26 -0700

    f1

--- _printf.c
+++ _printf.c
@@ -1,44 +1,37 @@
 #include "main.h"
 /**
- * -this task 0 code 
+ * -this task 0 code
  *  */
 int _printf(const char *format, ...)
 {
 	unsigned int i, s_count, count = 0;
-	
 	va_list agr;
-	
-	va_start(agr,format);
-	
+	va_start(agr, format);
 	for (i = 0;  format[1] != '\0'; i++)
 	{
 		if (format[i] != '%')
 		{
 			putchar(format[i]);
-		
 		}
-		else if (format [i + 1] == 'c')
+		else if (format[i + 1] == 'c')
 		{
 			putchar(va_arg(agr, int));
 			i++;
 		}
-		else if (format[i + 1]== 's')
+		else if (format[i + 1] == 's')
 		{
-			s_count = puts(va_arg(agr,char *));
+			s_count = puts(va_arg(agr, char *));
 			i++;
-			count += (s_count -1);
+			count += (s_count - 1);
 		}
-		else if (format [i + 1] == '%' )
+		else if (format[i + 1] == '%')
 		{
 			putchar('%');
 		}
 		count += 1;
-	}	
-	
+	}
 	va_end (agr);
 	return (count);
-
-
-}	
+}
 
 
